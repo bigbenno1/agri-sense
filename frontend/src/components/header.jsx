a4f1fb0c1d5d26ec68379555e61fd2bbdd4b8bd8
Ben Estacio <benjamin.estacio@gmail.com>
2025-11-04 00:20:22 -0600

    Progress on header component

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,42 +1,33 @@
 import React from 'react';
+import "./header.css"
+
 export default function Header() {
     return (
         <>
-        
-        <div style={{
-            backgroundColor: "#CDDECB", 
-            borderStyle: "groove none none none", 
-            boxShadow: "0px 1px 5px",
-            padding:"0px", 
-            width:"100%",
-            height:"8vh",
-            display:"flex",
-            alignItems:"center",
-            justifyContent:"center",
-            position: "fixed"}}>
+        {/* Font/icon links */}
+        <link href="https://fonts.googleapis.com/css?family=Offside" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet"></link>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
 
-            {/* Font/icon links */}
-            <link href="https://fonts.googleapis.com/css?family=Offside" rel="stylesheet"></link>
-            <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet"></link>
-            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
+        <header className="header-box">
+            {/* Menu button */}
+            <a href="">
+                <i className="menu-button material-icons">menu</i>
+            </a>
+
+            {/* Title */}
+            <a className="title"
+                href="">
+                <h1>🌱Agri-Sense🌱</h1>
+            </a>
             
-            <header>
-                {/* Title */}
-                <div style={{
-                    fontFamily: 'Offside, sans-serif', 
-                    textAlign: 'Center',
-                    color:"#537E72",
-                    textShadow:"0px 2.5px white,  -.5px 2.5px white, .5px 2.5px white, -.5px 0px white, .5px 0px white, 0px 0px 3px white",
-                    paddingTop: "0px",
-                    fontSize: "2.5vh"}}>
-                    <h1>🌱Agri-Sense🌱</h1>
-                    
-                
-                </div>
-                
-                
-            </header>
-        </div>
+            {/* Profile Icon */}
+            <a href="">
+                <i class="material-icons profile-button">
+                    account_circle
+                </i>
+            </a>
+        </header>
         </>
     )
 }
