79fe80346c502a59b44b9a954ed769e655e94d93
Blake Salbilla <blake.salbilla@gmail.com>
2025-11-10 08:56:38 -0600

    Create light reading function

--- hardware/data_reader.ino
+++ hardware/data_reader.ino
@@ -6,6 +6,8 @@
 
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <Wire.h>
+#include <BH1750.h>
 
 // Data wire is conntec to the Arduino digital pin 4
 #define ONE_WIRE_BUS 4
@@ -22,12 +24,14 @@ void setup(void)
   Serial.begin(9600);
   // Start up the library
   sensors.begin();
+  Wire.begin();
+  lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE);
 }
 
 void loop(void){ 
   // Call different reading functions below here
   temperature();
-
+  lightData();
 
   delay(250);
 }
@@ -37,4 +41,20 @@ void temperature(void) {
   sensors.requestTemperatures(); 
   float farenheitTemp = sensors.getTempFByIndex(0);
   Serial.println(farenheitTemp);
+}
+
+// Function to read and print light data
+void lightData(void)
+{
+  float lux = lightMeter.readLightLevel();
+
+  Serial.print("Light level | Pin: A4/A5 | Units: PPFD | Value: ");
+  Serial.println(luxToPPFD(lux));
+}
+
+// Function to convert lux to PPFD
+float luxToPPFD(float lux)
+{
+  float kConversionFactor = 0.0185;
+  return lux * kConversionFactor;
 }
