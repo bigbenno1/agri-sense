8b45f4bcedc99632b8b1981da48fad9ad1942917
SirScotterington <scott7gilbert@gmail.com>
2025-11-12 20:44:36 -0600

    Move humidity/temp to correct file

--- hardware/data_reader/data_reader.ino
+++ hardware/data_reader/data_reader.ino
@@ -2,6 +2,11 @@
 #include <DallasTemperature.h>
 #include <Wire.h>
 #include <BH1750.h>
+#include "DHT.h"
+#define DHTPIN 2     // Digital pin connected to the DHT sensor (set to 2 for now)
+
+#define DHTTYPE DHT11   // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
 
 // Data wire is conntec to the Arduino digital pin 4
 #define ONE_WIRE_BUS 4
@@ -22,6 +27,7 @@ void setup(void)
   sensors.begin();
   Wire.begin();
   lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE);
+  dht.begin();
 }
 
 void loop(void){ 
@@ -56,4 +62,52 @@ float luxToPPFD(float lux)
 {
   float kConversionFactor = 0.0185;
   return lux * kConversionFactor;
+}
+
+void humidity(void)
+{
+  // Placeholder for humidity reading function
+
+  // Sensor readings may also be up to 2 seconds
+
+  float h = dht.readHumidity();
+
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // CODE FOR HEAT INDEX IN CASE
+  // Compute heat index in Fahrenheit (the default)
+  //float hif = dht.computeHeatIndex(f, h);
+
+  // Compute heat index in Celsius (isFahreheit = false)
+  //float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.println(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F(" %"));
+  Serial.println("---");
+  Serial.println(F("Air temperature: "));
+  Serial.print(t);
+  Serial.print(F(" °C / "));
+  Serial.print(f);
+  Serial.print(F(" °F"));
+  Serial.println("---");
+
+  //HEAT INDEX OUTPUT
+  /*
+  Serial.println(F("Heat index: "));
+  Serial.print(hic);
+  Serial.print(F(" °C / "));
+  Serial.print(hif);
+  Serial.print(F(" °F"));
+  */
 }
